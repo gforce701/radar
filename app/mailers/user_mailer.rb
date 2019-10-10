e78ec3c248682fb9ea5e9b7004f5bef091da84cc
gforce701 <56001837+gforce701@users.noreply.github.com>
2019-10-09 22:29:11 -0700

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
     @url = token_session_url(token: token)
     @user = user
 
-    mail(to: @user.email, subject: 'Log in to Klaxon')
+    mail(to: @user.email, subject: 'Log in to i2b-Radar')
   end
 
   def welcome_email(user: nil, invited_by: nil)
@@ -15,7 +15,7 @@ class UserMailer < ApplicationMailer
     @url = token_session_url(token: token)
     @user = user
 
-    mail(to: @user.email, subject: 'Welcome to Klaxon!')
+    mail(to: @user.email, subject: 'Welcome to i2b-Radar!')
   end
 
 end
