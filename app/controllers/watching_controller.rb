1215a03145a00bbc3a79ff447fa85f5a34bb69a3
gforce701 <56001837+gforce701@users.noreply.github.com>
2020-10-05 08:58:40 -0500

    Update watching_controller.rb

--- app/controllers/watching_controller.rb
+++ app/controllers/watching_controller.rb
@@ -1,9 +1,8 @@
+  
 class WatchingController < ApplicationController
-
   before_action :authorize
 
   def feed
-    @changes = Change.order('created_at DESC').first(20)
+    @changes = Change.order('created_at DESC').first(40)
   end
-
 end
